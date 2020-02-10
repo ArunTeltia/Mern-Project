0553ad3935e37020a80bd90a6d44e02c4989cf60
ArunTeltia <teltia.arun18@gmail.com>
2020-02-10 08:01:08 +0530

    Create Project Structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import {BrowserRouter as Router,Route } from 'react-router-dom'; 
-
+import React from "react";
+import { BrowserRouter as Router, Route, Redirect ,Switch} from "react-router-dom";
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 function App() {
   return (
     <Router>
-      <Route>
-
-      </Route>
+      <Switch>
+        <Route path="/" exact={true}>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact={true}>
+          <NewPlace />
+        </Route>
+        <Redirect to="/"></Redirect>
+      </Switch>
     </Router>
   );
 }
